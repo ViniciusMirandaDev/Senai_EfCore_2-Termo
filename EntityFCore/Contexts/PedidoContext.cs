b64b36c28a16dc2e168dafa931526e4e39a3fd8c
FIC <FIC@info.local>
2020-09-09 19:05:42 -0300

    ORM Aula 06 feita

---
+++ EntityFCore/Contexts/PedidoContext.cs
@@ -0,0 +1,26 @@
+﻿using EntityFCore.Domais;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFCore.Contexts
+{
+    public class PedidoContext : DbContext
+    {
+        //Domains criados
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<PedidoItem> PedidosItens { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=LAB107201\SQLEXPRESS2;Initial Catalog = EntityFCore ;user id = sa;password = sa132");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ EntityFCore/Migrations/20200909211659_AlterTablePedido.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFCore.Migrations
+{
+    public partial class AlterTablePedido : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "status",
+                table: "Pedidos",
+                newName: "Status");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Pedidos",
+                newName: "status");
+        }
+    }
+}
